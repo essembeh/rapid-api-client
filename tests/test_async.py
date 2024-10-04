8e24eed7c6f14b79d921e38748de6f2d5dedc756
Sébastien MB <seb@essembeh.org>
2024-10-04 07:53:29 +0200

    ✅ Add asyncio gather test

--- tests/test_async.py
+++ tests/test_async.py
@@ -7,12 +7,13 @@ from rapid_api_client import RapidApi, get
 from .conftest import Infos
 
 
+class HttpBinApi(RapidApi):
+    @get("/anything", response_class=Infos)
+    async def get(self): ...
+
+
 @mark.asyncio(loop_scope="module")
 async def test_taskgroup(client):
-    class HttpBinApi(RapidApi):
-        @get("/anything", response_class=Infos)
-        async def get(self): ...
-
     api = HttpBinApi(client)
 
     async with asyncio.TaskGroup() as tg:
@@ -21,4 +22,16 @@ async def test_taskgroup(client):
     assert len(tasks) == 3
     for task in tasks:
         infos = task.result()
+        assert infos.method == "GET"
+
+
+@mark.asyncio(loop_scope="module")
+async def test_gather(client):
+    api = HttpBinApi(client)
+
+    asyncio.gather()
+    tasks = await asyncio.gather(*[api.get() for _ in range(3)])
+
+    assert len(tasks) == 3
+    for infos in tasks:
         assert infos.method == "GET"
